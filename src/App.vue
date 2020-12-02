f1240dbe468019f2591988d6ad8c8fef59fd2bfc
Aerogirl <angelika.holod@gmail.com>
2020-12-03 00:40:29 +0100

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="app">
+    <div class="nav">
+      <RouterLink to="/home">spacer</RouterLink>
+      <RouterLink to="/about">about</RouterLink>
+    </div>
+<RouterView></RouterView>
+  </div>
+</template>
+
+<style lang="scss">
+
+</style>
+git init
+git add *
+git commit -a -m 'Initial commit'
+git remote add origin git@github.com:username/repo.git
---
+++ src/views/Home.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="home">
+    <h1>Spacer</h1>
+    <p>sasvs</p>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Home',
+};
+</script>
